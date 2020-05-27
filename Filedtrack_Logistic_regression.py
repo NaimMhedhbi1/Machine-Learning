cd705961705fcebc5570f278736227523b1b7624
121naim <56754920+121naim@users.noreply.github.com>
2020-05-27 23:39:27 +0100

    Update and rename 121Filedtrack_Logistic_regression.py to Filedtrack_Logistic_regression.py

--- Filedtrack_Logistic_regression.py
+++ Filedtrack_Logistic_regression.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import seaborn as sns
-df = pd.read_excel('C://Users/121//.spyder-py3//EDA projects//121_field_track_logistic_regression.xlsx')
+df = pd.read_excel('field_track_logistic_regression.xlsx')
 X = df[[' qst sans num de TEL ','score']]
 y = df['score_labeled']
 
@@ -17,7 +17,7 @@ sns.heatmap(confusion_matrix, annot=True)
 print('Accuracy: ',metrics.accuracy_score(y_test, y_pred))
 Accuracy = metrics.accuracy_score(y_test, y_pred)
 
-df2 = pd.read_excel('C:/Users/121/.spyder-py3/EDA projects/121_field_track_logistic_regression_test.xlsx')
+df2 = pd.read_excel('field_track_logistic_regression_test.xlsx')
 
 y_pred = logistic_regression.predict(df2)
 
