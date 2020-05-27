02541afa0859b0eba9c0ed12c70c375e664c7e2b
121naim <56754920+121naim@users.noreply.github.com>
2020-05-27 23:40:55 +0100

    Update and rename EDA121-AI.py to AI.py

--- AI.py
+++ AI.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt #visualisation
 from sklearn.preprocessing import StandardScaler
 sns.set(color_codes=True)
  
-df_initial = pd.read_excel('C:/Users/121/.spyder-py3/EDA projects/AI_2_12.xlsx')
+df_initial = pd.read_excel('AI_2_12.xlsx')
 df_object = df_initial.select_dtypes(include = ['object'])
 df = df_initial.select_dtypes(include = ['float64','int64'])
 
